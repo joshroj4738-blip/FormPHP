840bace3c52dcb5e8a3b4bb97f23924bbb4a8431
joshroj4738-blip <joshroj4738@gmail.com>
2025-08-12 16:50:02 -0500

    Update Información.php

--- Información.php
+++ Información.php
@@ -1,1 +1,12 @@
+<?php 
+$Identificacion = $_POST ['Identificación']; 
+$NomApellidos = $_POST ['NomApellidos']; 
+$Cargo = $_POST ['Cargo'];
+$Salario = $_POST ['Salario'];
+echo "La identificación del Empleado es:".Identificación;
+echo "Los nombres y apellidos del Empleado es:".NomApellidos;
+echo "El cargo del  Empleado es:".Cargo;
+echo "El salario del Empleado es:".Salario;
 
+
+?>
