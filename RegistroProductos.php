8c54fb82a4ff2b9ecc9672d645bc5bc3695c7109
joshroj4738-blip <joshroj4738@gmail.com>
2025-08-19 17:55:38 -0500

    Update RegistroProductos.php

--- RegistroProductos.php
+++ RegistroProductos.php
@@ -1,4 +1,10 @@
 <?php
+$producto1 = $_POST ['Product1'];
+$producto2 = $_POST ['Product2'];
+$producto3 = $_POST ['Product3'];
+$Valor1 = $_POST ['Cost1'];
+$Valor2 = $_POST ['Cost2']; 
+$Valor3 = $_POST ['Cost3'];
 
-
+echo ""
 ?>
