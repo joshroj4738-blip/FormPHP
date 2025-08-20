88f3899c300f9b8676e987c7e02cc24530cabc86
joshroj4738-blip <joshroj4738@gmail.com>
2025-08-20 17:19:02 -0500

    Update RegistroProductos.php

--- RegistroProductos.php
+++ RegistroProductos.php
@@ -6,5 +6,18 @@ $Valor1 = $_POST ['Cost1'];
 $Valor2 = $_POST ['Cost2']; 
 $Valor3 = $_POST ['Cost3'];
 
-echo ""
+echo "El primer producto es ".$producto1;
+echo "<br>";
+echo "El precio correspondiente es ". $Valor1; 
+echo "<br>"; 
+echo "El segundo producto es ".$producto2;
+echo "<br>";
+echo "El precio correspondiente es ". $Valor2;
+echo "<br>";
+echo "El tercer producto es ".$producto3;
+echo "<br>";
+echo "El precio correspondiente es ". $Valor3;
+echo "<br>";
+
+
 ?>
